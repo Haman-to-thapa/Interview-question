c91f2cc4763e3899251bdf9d6e397e43e2ecb84d
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-01-31 08:30:25 +0530

    project-2

---
+++ project-two/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  let str1 = "krishna";
+  let str2 = "menu";
+
+  // Find the max length to iterate safely
+  let maxLength = Math.max(str1.length, str2.length);
+
+  // Merge characters from both strings
+  let mergedArray = [];
+  for (let i = 0; i < maxLength; i++) {
+    if (i < str1.length) mergedArray.push(str1[i]);
+    if (i < str2.length) mergedArray.push(str2[i]);
+  }
+
+  return (
+    <div>
+      {mergedArray.map((char, index) => (
+        <p key={index}>{char}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
+
